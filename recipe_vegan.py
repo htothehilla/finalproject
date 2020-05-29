f99a744544d567a308f74b6b52039d8da36343f7
Hillary K Juma <hillarykjuma@Hillarys-Air.cust.communityfibre.co.uk>
2020-05-29 15:20:10 +0100

    Initial commit

---
+++ recipe_vegan.py
@@ -0,0 +1,37 @@
+# install dependent libraries
+# you will have to pip install requests when creating a new enviroments via the terminal
+import requests
+# define a function that calls the api with
+
+#to note api_id and api_key is part of the url
+api_id = "1c7857d6"
+api_key = "2ac1fd3542014ae0f08dee149581dae5"
+
+#asking user how much time they have
+time = input("how much time in minutes do you have to cook ?")
+
+def recipe_search(ingredient):
+    url = 'https://api.edamam.com/search?q={}&app_id={}&app_key={}&from=0&to=10&health=vegan&time={}'.format(
+        ingredient, api_id, api_key,time)
+    response = requests.get(url)
+    recipe = response.json()
+    return recipe['hits']
+
+# define a function that asks a user what ingredient they would like
+
+def run():
+    question = input('What ingredient do you have to cook with ? : ')
+    recipe_response = recipe_search(question)
+
+    # save results to a file
+
+    with open('results.txt', 'w+') as recipe_file:
+        for recipe in recipe_response:
+            recipe_file.write(recipe['recipe']['label'] + '\n')
+            recipe_file.write(recipe['recipe']['url'] + '\n')
+            recipe_file.write(str(recipe['recipe']['healthLabels']) + '\n')
+            recipe_file.write(str(recipe['recipe']['totalTime']) + '\n')
+            recipe_file.write('\n')
+
+run()
+
